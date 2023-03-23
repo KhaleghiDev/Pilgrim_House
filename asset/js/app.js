99878304dd91e76b1319870fd46f1193c3cd4306
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2023-03-23 19:32:35 +0430

    add modal and keyup esc to projeact

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,17 +1,43 @@
 const app = Vue.createApp({
-    
-    data() {
-      return {
-        title: sample.title,
-        address:sample.address,
-        body:sample.about,
-        amenities:sample.amenities,
-        prices:sample.prices,
-        headerImage:{
-          'background-image':"url('asset/images/header.jpeg')"
-        }
-        
+
+  data() {
+    return {
+      droupdone: true,
+      title: sample.title,
+      address: sample.address,
+      body: sample.about,
+      amenities: sample.amenities,
+      prices: sample.prices,
+      modalShow: false,
+      headerImage: {
+        'background-image': "url('asset/images/header.jpeg')"
+      }
+
+    }
+  },
+  methods: {
+    escCloseModal: function (evt) {
+      if (evt.keyCode === 27 && this.modalShow) {
+        this.modalShow = false
       }
     }
-  })
-  app.mount('#app')
+  },
+  created : function () {
+    document.addEventListener('keyup', this.escCloseModal)
+  },
+  destroyed() {
+    document.removeEventListener('keyup', this.escCloseModal)
+
+  },
+  watch: {
+    modalShow: function () {
+      var className = 'modal-open';
+      if (this.modalShow) {
+        document.body.classList.add('modal-open')
+      } else {
+        document.body.classList.remove('modal-open')
+      }
+    }
+  }
+})
+app.mount('#app')
